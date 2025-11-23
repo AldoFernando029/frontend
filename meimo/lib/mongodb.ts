a70586764c4e947bf215c32ec69613c82b05b001
AldoFernando029 <aldofernando2006@gmail.com>
2025-11-23 14:20:29 +0700

    perbaikan lagi

--- meimo/lib/mongodb.ts
+++ meimo/lib/mongodb.ts
@@ -1,19 +1,11 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI!;
-const options = {};
-
 let client;
-let clientPromise: Promise<MongoClient>;
-
-declare global {
-  // agar TS tidak error
-  // eslint-disable-next-line no-var
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
-}
+let clientPromise;
 
 if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri);
   global._mongoClientPromise = client.connect();
 }
 

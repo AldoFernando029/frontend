04897145cdbbc86bf63b2b49c5874eebebb6ad8d
AldoFernando029 <aldofernando2006@gmail.com>
2025-11-23 16:12:18 +0700

    perbaikan lagi login nya

--- meimo/app/login/layout.tsx
+++ meimo/app/login/layout.tsx
@@ -5,22 +5,10 @@ export const metadata: Metadata = {
   description: "Halaman login untuk website Rasa Manado",
 };
 
-export default function LoginLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function LoginLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          overflow: "hidden", 
-        }}
-      >
-        {children}
-      </body>
-    </html>
+    <div style={{ width: "100%", height: "100%", margin: 0, padding: 0 }}>
+      {children}
+    </div>
   );
 }
